d961cf039d0cda019ae210cd83543c35cdf8cb8f
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-11 11:16:00 -0700

    updates packages, adds cards for keeps

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -2,14 +2,25 @@
   <div class="home row">
     <div class="col-12">
       <h1>KEEPR</h1>
-      {{ publicKeeps }}
+      <div class="row">
+        <keep
+          class="col-3"
+          v-for="publicKeep in publicKeeps"
+          :key="publicKeep.Id"
+          :keepData="publicKeep"
+        ></keep>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Keep from "@/components/Keep.vue";
 export default {
   name: "home",
+  components: {
+    Keep
+  },
   mounted() {
     this.$store.dispatch("getPublicKeeps");
   },
