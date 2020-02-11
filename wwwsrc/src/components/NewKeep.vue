70ec0abb4512b4ed14e7db655acb14623e813056
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-11 16:23:00 -0700

    modal styling to keeps, vaults

--- wwwsrc/src/components/NewKeep.vue
+++ wwwsrc/src/components/NewKeep.vue
@@ -16,7 +16,7 @@
       aria-labelledby="newKeepModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-sm" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="newKeepModalLabel">New Keep</h5>
