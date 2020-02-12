70ffc16276e8f135181db4f6092fb3b62d959dfc
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-12 11:37:13 -0700

    adds dropdown to keeps displaying available vaults

--- wwwsrc/src/components/Keep.vue
+++ wwwsrc/src/components/Keep.vue
@@ -4,6 +4,16 @@
       <div id="keep-hover" @click.prevent="setActiveKeep">
         <img :src="keepData.img" class="card-img-top rounded" alt="..." />
       </div>
+      <div v-if="$auth.isAuthenticated" class="dropdown text-right">
+        <button class="dropdown-toggle" data-toggle="dropdown">
+          <i class="fas fa-plus text-right">
+            <!-- list of vaults here, only visible if user is authenticated -->
+          </i>
+        </button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" v-for="vault in vaults" :key="vault.id" href="#">{{ vault.name }}</a>
+        </div>
+      </div>
       <div class="card-body">
         <p class="card-text">
           <i class="far fa-eye"></i>
@@ -23,6 +33,14 @@
 export default {
   name: "Keep",
   props: ["keepData"],
+  mounted() {
+    this.$store.dispatch("getVaults");
+  },
+  computed: {
+    vaults() {
+      return this.$store.state.vaults;
+    }
+  },
   methods: {
     setActiveKeep() {
       // NOTE reroutes user to activeKeep/KeepDetails
