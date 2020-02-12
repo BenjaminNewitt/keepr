e2bccb17493177a3b07b55abaa3dbfc8ee2e8844
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-12 10:41:49 -0700

    draws public keeps to keepDetails

--- wwwsrc/src/views/KeepDetails.vue
+++ wwwsrc/src/views/KeepDetails.vue
@@ -18,6 +18,17 @@
             <h2>{{ activeKeep.name }}</h2>
             <p>{{activeKeep.description }}</p>
           </div>
+          <div class="col-12">
+            <h4>More Keeps</h4>
+            <div class="row">
+              <keep
+                class="col-3 align-col"
+                v-for="publicKeep in publicKeeps"
+                :key="publicKeep.Id"
+                :keepData="publicKeep"
+              ></keep>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -25,16 +36,28 @@
 </template>
 
 <script>
+import Keep from "@/components/Keep.vue";
 export default {
   name: "keepDetails",
-  mounted() {},
+  components: {
+    Keep
+  },
+  mounted() {
+    this.$store.dispatch("getPublicKeeps");
+  },
   computed: {
     activeKeep() {
       return this.$store.state.activeKeep;
+    },
+    publicKeeps() {
+      return this.$store.state.publicKeeps;
     }
   }
 };
 </script>
 
-<style>
+<style scoped>
+.align-col {
+  text-align: -webkit-center;
+}
 </style>
