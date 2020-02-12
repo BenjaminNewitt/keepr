d17f6992236d0d676c7c03479f1cc3a0b1792c6f
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-12 13:31:23 -0700

    formats vaultData in vaultDetails

--- wwwsrc/src/views/VaultDetails.vue
+++ wwwsrc/src/views/VaultDetails.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="vault-details" class="container-fluid">
     <div class="row">
-      <div class="col-12">{{ activeVault }}</div>
-      <div class="col-12">{{ vaultKeeps }}</div>
+      <div class="col-12">
+        <h2>{{ activeVault.name }}</h2>
+        <p>{{ activeVault.description }}</p>
+      </div>
+      <div class="col-12 align-col">
+        <div class="row">
+          <keep
+            class="col-3 align-col"
+            v-for="vaultKeep in vaultKeeps"
+            :key="vaultKeep.Id"
+            :keepData="vaultKeep"
+          ></keep>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Keep from "@/components/Keep.vue";
 export default {
   name: "vaultDetails",
+  components: {
+    Keep
+  },
   mounted() {
     this.$store.dispatch("getKeepsByVaultId", this.activeVault.id);
   },
