e9503598607c397af6824579430439a79474f8bb
Benjamin Newitt <benjaminnewitt@gmail.com>
2020-02-12 10:51:20 -0700

    vaultKeeps added to vaultDetails view

--- wwwsrc/src/views/VaultDetails.vue
+++ wwwsrc/src/views/VaultDetails.vue
@@ -2,6 +2,7 @@
   <div id="vault-details" class="container-fluid">
     <div class="row">
       <div class="col-12">{{ activeVault }}</div>
+      <div class="col-12">{{ vaultKeeps }}</div>
     </div>
   </div>
 </template>
@@ -9,9 +10,15 @@
 <script>
 export default {
   name: "vaultDetails",
+  mounted() {
+    this.$store.dispatch("getKeepsByVaultId", this.activeVault.id);
+  },
   computed: {
     activeVault() {
       return this.$store.state.activeVault;
+    },
+    vaultKeeps() {
+      return this.$store.state.vaultKeeps;
     }
   }
 };
